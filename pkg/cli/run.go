9413d51847393a561e9c28e12570b4d6073cb575
Tamer Aly <taly.dev75@gmail.com>
2021-08-23 19:08:00 -0400

    add customizable line ending flags that default to the system line ending

--- pkg/cli/run.go
+++ pkg/cli/run.go
@@ -2,14 +2,17 @@ package cli
 
 import ( 
 	"bufio"
+	"errors"
 	"io"
 	"flag"
 	"fmt"
 	"os"
+	"runtime"
 )
 
 type Args struct {
 	IsHelp bool
+	LineEnding string
 }
 
 func init() {
@@ -19,17 +22,37 @@ func init() {
 	}
 }
 
-func ParseArgs() *Args {
+func ParseArgs() (*Args, error) {
 	helpPtr := flag.Bool("h", false, "displays this help message")
+	lfPtr := flag.Bool("lf", false, "force Linux style \\n input newline")
+	crlfPtr := flag.Bool("crlf", false, "force Windows style \\r\\n input newline")
 
 	flag.Parse()
 
+	if *lfPtr && *crlfPtr {
+		return nil, errors.New("line endings cannot be both \\n and \\r\\n")
+	} 
+		
+	var lineEnding string;
+	if *lfPtr != *crlfPtr {
+		if *lfPtr {
+			lineEnding = "\n"
+		} else {
+			lineEnding = "\r\n"
+		}
+	} else {
+		// both are false, default to system newline
+		lineEnding = determineSystemNewline()
+	}
+	
+
 	return &Args {
 		IsHelp: *helpPtr,
-	}
+		LineEnding: lineEnding,
+	}, nil
 }
 
-func PrintOneLine(r io.Reader, w io.Writer) error {
+func PrintOneLine(r io.Reader, w io.Writer, lineEnding string) error {
 	const READ_BUFFER_LEN = 64 * 1024
 
 	reader := bufio.NewReader(r)
@@ -47,9 +70,9 @@ func PrintOneLine(r io.Reader, w io.Writer) error {
 		// array.
 		var writeBuffer []byte = nil;
 		if n == READ_BUFFER_LEN {
-			writeBuffer = removeNewline(readBuffer, n)
+			writeBuffer = removeNewline(readBuffer, n, lineEnding)
 		} else {
-			writeBuffer = removeNewline(readBuffer[:n], n)
+			writeBuffer = removeNewline(readBuffer[:n], n, lineEnding)
 		}
 		w.Write(writeBuffer)
 	}
@@ -58,13 +81,18 @@ func PrintOneLine(r io.Reader, w io.Writer) error {
 }
 
 func Run(cmdLine []string) int {
-	args := ParseArgs()
+	args, err := ParseArgs()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+		return 1
+	}
+
 	if args.IsHelp {
 		flag.Usage()
 		return 0
 	}
 
-	if err := PrintOneLine(os.Stdin, os.Stdout); err != nil {
+	if err := PrintOneLine(os.Stdin, os.Stdout, args.LineEnding); err != nil {
 		fmt.Fprintf(os.Stderr, "%q", err)
 		return 1
 	}
@@ -72,7 +100,41 @@ func Run(cmdLine []string) int {
 	return 0
 }
 
-func removeNewline(buf []byte, n int) []byte {
+func determineSystemNewline() string {
+	if runtime.GOOS == "windows" {
+		return "\r\n"
+	}
+
+	return "\n";
+}
+
+func removeNewline(buf []byte, n int, newLine string) []byte {
+	if newLine == "\r\n" {
+		return removeCRLF(buf, n)
+	} else {
+		return removeLF(buf, n)
+	}
+}
+
+func removeCRLF(buf []byte, n int) []byte {
+	out := make([]byte, 0, n)
+	for i := 0; i < n; i++ {
+		if (buf[i] == '\r') {
+			if next := i + 1; next < n && buf[next] == '\n' {
+				out = append(out, ' ')
+				i++; // skip the '\n'
+			} else {
+				out = append(out, '\r')
+			}
+		} else {
+			out = append(out, buf[i])
+		}
+	}
+
+	return out
+}
+
+func removeLF(buf []byte, n int) []byte {
 	for i := 0; i < n; i++ {
 		if buf[i] == '\n' {
 			buf[i] = ' '
--- pkg/cli/run_test.go
+++ pkg/cli/run_test.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-func TestPrintOneLine(t *testing.T) {
+func TestPrintOneLine_LFText_LF(t *testing.T) {
     testCases := map[string]string {
 		" hello ": " hello ",
 		"\nhello": " hello",
@@ -21,7 +21,82 @@ func TestPrintOneLine(t *testing.T) {
 		reader := strings.NewReader(input)
 		writer := bytes.NewBufferString("")
 
-		if err := PrintOneLine(reader, writer); err != nil {
+		if err := PrintOneLine(reader, writer, "\n"); err != nil {
+			t.Fatal(err)
+		}
+
+		if actual := writer.String(); actual != expected {
+			t.Fatalf("expected '%s', got '%s'", expected, actual)
+		}
+	}
+}
+
+func TestPrintOneLine_CRLFText_CRLF(t *testing.T) {
+    testCases := map[string]string {
+		" hello ": " hello ",
+		"\r\nhello": " hello",
+		"": "",
+		"foo": "foo",
+		"f": "f",
+		"\r\n": " ",
+		"hello\r\n ": "hello  ",
+	}
+
+	for input, expected := range testCases {
+		reader := strings.NewReader(input)
+		writer := bytes.NewBufferString("")
+
+		if err := PrintOneLine(reader, writer, "\r\n"); err != nil {
+			t.Fatal(err)
+		}
+
+		if actual := writer.String(); actual != expected {
+			t.Fatalf("expected '%s', got '%s'", expected, actual)
+		}
+	}
+}
+
+func TestPrintOneLine_CRLFText_LF(t *testing.T) {
+    testCases := map[string]string {
+		" hello ": " hello ",
+		"\r\nhello": "\r hello",
+		"": "",
+		"foo": "foo",
+		"f": "f",
+		"\r\n": "\r ",
+		"hello\r\n ": "hello\r  ",
+	}
+
+	for input, expected := range testCases {
+		reader := strings.NewReader(input)
+		writer := bytes.NewBufferString("")
+
+		if err := PrintOneLine(reader, writer, "\n"); err != nil {
+			t.Fatal(err)
+		}
+
+		if actual := writer.String(); actual != expected {
+			t.Fatalf("expected '%s', got '%s'", expected, actual)
+		}
+	}
+}
+
+func TestPrintOneLine_LFText_CRLF(t *testing.T) {
+    testCases := map[string]string {
+		" hello ": " hello ",
+		"\nhello": "\nhello",
+		"": "",
+		"foo": "foo",
+		"f": "f",
+		"\n": "\n",
+		"hello\n ": "hello\n ",
+	}
+
+	for input, expected := range testCases {
+		reader := strings.NewReader(input)
+		writer := bytes.NewBufferString("")
+
+		if err := PrintOneLine(reader, writer, "\r\n"); err != nil {
 			t.Fatal(err)
 		}
 
